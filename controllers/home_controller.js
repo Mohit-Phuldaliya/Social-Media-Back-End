3708e883e102db10393f7c5cd3447a6339e09ff7
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-02-17 04:16:59 +0530

    Setup Different  Router and Controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function (req, res) {
+  return res.end("<h1>Express is up</h1>");
+};
+// this function is similar to function to that action in app.get wala function
---
+++ controllers/users_controller.js
@@ -0,0 +1,4 @@
+module.exports.profile = function (req, res) {
+  return res.end("<h1>User Profile</h1>");
+};
+// now this is ready to access by router now that route need to be accessed by my browser
