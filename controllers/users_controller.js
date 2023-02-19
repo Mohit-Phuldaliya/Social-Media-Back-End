d9b31617dff20808a19bcaf74cce45f4acb8e7ed
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-02-19 07:38:58 +0530

    Sign Up page setup

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req, res) {
   return res.end("<h1>User Profile</h1>");
 };
@@ -19,8 +21,34 @@ module.exports.signIn = function (req, res) {
 
 // get the sign up data
 module.exports.create = function (req, res) {
-  // to do
+  // check wether password and confirm password are equal or not if not then redirect to signup page
+  if (req.body.password != req.body.confirm_password) {
+    return req.redirect("back");
+  }
+
+  // try to find out the user id if it is exist then we does not create it and if it is does not exist then we create it
+  User.findOne({ name: req.body.name }, function (err, user) {
+    if (err) {
+      console.log("Error in finding user in signing up'");
+      return;
+    }
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user while signing up");
+          return;
+        }
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      //when user is already present
+      return req.redirect("back");
+    }
+  });
 };
+
+//
+//
 
 // sign in and create the session for the user
 module.exports.createSession = function (req, res) {
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const app = express();
 const port = 8000;
+
+const db = require("./config/mongoose");
+
+const app = express();
 
 app.use(express.urlencoded()); // reading through the post request
 
