ae54d89923592939074051a8722bdd15989bedad
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-02-21 04:12:56 +0530

    Making pages sign-in and sign-up only accessible when user Signed-OUT

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -30,6 +30,11 @@ module.exports.profile = function (req, res) {
 
 // render the sign up page
 module.exports.signUp = function (req, res) {
+  //making accesible sign-up page only when the user is Signed-OUT
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+
   return res.render("user_sign_up", {
     title: "Social | Sign Up",
   });
@@ -37,6 +42,11 @@ module.exports.signUp = function (req, res) {
 
 // render the sign in page
 module.exports.signIn = function (req, res) {
+  //making accesible sign-in page only when the user is Signed-OUT
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+
   return res.render("user_sign_in", {
     title: "Social | Sign In",
   });
