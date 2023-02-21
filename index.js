67473b56bf5e8d445e0f86ea242f99b8bfaf0daa
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-02-21 05:39:23 +0530

    Setting Up MongoStore for Session Cookie

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const passportLocal = require("./config/passport-local-strategy");
 
 const app = express();
 
+//setting mongo-store it reequires an argument session (express session) bcz u need to store the session information in the
+// const MongoStore = require("connect-mongo")(session);
+const connectMongo = require("connect-mongo");
+const MongoStore = connectMongo(session);
+
 app.use(express.urlencoded()); // reading through the post request
 
 app.use(cookieParser()); // setting up the cookie parser
@@ -33,6 +38,19 @@ app.use(
       //we need give age to cookie for how long this should be valid after that, that cookie expires that session cookie expires
       maxAge: 1000 * 60 * 100,
     },
+    store: new MongoStore(
+      // set up mongo Store
+      //mongo store is used to store the session cookie in the db
+      {
+        // setting mongoose connection so it interact with mongoose
+        mongooseConnection: db,
+        // do i want to remove this automatically ? no.
+        autoRemove: "disabled",
+      }, // now there's a callback function in case the connection is not established
+      function (err) {
+        console.log(err || "connect--mongodb setup ok");
+      }
+    ),
   })
 );
 // we need to tell app to use passport
