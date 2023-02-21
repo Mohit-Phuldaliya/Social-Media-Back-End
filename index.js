b7795c1d5dc80f09fff2bfee92103cf0944b46a6
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-02-22 00:33:20 +0530

    Set up Static files and their acces for layout and pages

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const expressLayouts = require("express-ejs-layouts");
 // telling app to use layouts
 app.use(expressLayouts);
 
+// extract style and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+// need to tell in which folder app should look for static file
+app.use(express.static("./assets"));
+
 // connection DB
 const db = require("./config/mongoose");
 
