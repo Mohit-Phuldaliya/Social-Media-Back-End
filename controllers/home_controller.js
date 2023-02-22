cb216d7569b491b109d319840ae8f244c434f7dd
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-02-22 06:15:02 +0530

    Display Posts and Related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,32 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
-  console.log(req.cookies);
-  res.cookie("user_id", 25);
-  return res.render("home", {
-    title: "Home",
-  });
+  // console.log(req.cookies);
+  // res.cookie("user_id", 25);
+
+  // find all post
+  // this query will return all the post
+
+  //fetching all post with user id only
+  //
+  // Post.find({}, function (err, posts) {
+  //   return res.render("home", {
+  //     title: "SocialMedia Home",
+  //     //passing all posts in home views
+  //     posts: posts,
+  //   });
+  // });
+  ///
+
+  //fetching all post with user object by populating whole user from database using method populated provided by mongoose we use .exec() to write our callback function
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "SocialMedia Home",
+        //passing all posts in home views
+        posts: posts,
+      });
+    });
 };
 // this function is similar to function to that action in app.get wala function
