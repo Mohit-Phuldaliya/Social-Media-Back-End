a43a4b2482e4f30bd1a17bb72f277119565f26fd
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-03-06 01:48:51 +0530

    Chat Box Using Socket.io

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,6 +1,7 @@
 //frontend file for socket.io
 // creating a class for creating functionality
-class ChatEngine {
+
+class MyChatEngines {
   constructor(chatBoxId, userEmail) {
     this.chatBox = $(`#${chatBoxId}`);
     this.userEmail = userEmail;
@@ -25,12 +26,53 @@ class ChatEngine {
 
       self.socket.emit("join_room", {
         user_email: self.userEmail,
-        chatroom: "codeial",
+        chatroom: "phuldaliya",
       });
 
       self.socket.on("user_joined", function (data) {
         console.log("a user joined!", data);
       });
+    });
+
+    // CHANGE :: send a message on clicking the send message button
+    $("#send-message").click(function () {
+      let msg = $("#chat-message-input").val();
+
+      if (msg != "") {
+        self.socket.emit("send_message", {
+          message: msg,
+          user_email: self.userEmail,
+          chatroom: "phuldaliya",
+        });
+      }
+    });
+
+    self.socket.on("receive_message", function (data) {
+      console.log("message received", data.message);
+
+      let newMessage = $("<li>");
+
+      let messageType = "other-message";
+
+      if (data.user_email == self.userEmail) {
+        messageType = "self-message";
+      }
+
+      newMessage.append(
+        $("<span>", {
+          html: data.message,
+        })
+      );
+
+      newMessage.append(
+        $("<sub>", {
+          html: data.user_email,
+        })
+      );
+
+      newMessage.addClass(messageType);
+
+      $("#chat-messages-list").append(newMessage);
     });
   }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -3,7 +3,14 @@
 
 module.exports.chatSockets = function (socketServer) {
   // io will be handling the connection
-  let io = require("socket.io")(socketServer);
+  let io = require("socket.io")(socketServer, {
+    cors: {
+      origin: "http://localhost:8000",
+      credentials: true,
+    },
+    // The reason for adding this code is to enable Cross-Origin Resource Sharing (CORS) between the frontend application running on port 8000 and the backend server running on port 5000.
+    // By default, browsers restrict cross-origin requests, which means that a web application running on one domain (e.g., http://localhost:8000) cannot normally access resources on another domain (e.g., http://localhost:5000). To allow cross-origin requests, the server must explicitly set the Access-Control-Allow-Origin header
+  });
 
   // whenever a connection request recieves it automatically will send back and acknowledgement to the frontend in connectionHandler automatically
   io.sockets.on("connection", function (socket) {
@@ -18,9 +25,14 @@ module.exports.chatSockets = function (socketServer) {
     socket.on("join_room", function (data) {
       console.log("joining request rec.", data);
 
-      socket.join(data.chatroom);
+      socket.join(data.chatroom); // if chatroom with name data.chatroom exist already then user is connected or enter into that chatroom if it doesnot exist it will creat chatroom and enter the use into it
 
-      io.in(data.chatroom).emit("user_joined", data);
+      io.in(data.chatroom).emit("user_joined", data); //telling chatroom that user enter in the chat room
+    });
+
+    // CHANGE :: detect send_message and broadcast to everyone in the room
+    socket.on("send_message", function (data) {
+      io.in(data.chatroom).emit("receive_message", data);
     });
   });
 };
